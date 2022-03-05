0774ec48793f71e3e3362d4506afb6e7726994a2
Sarah <22rubens@elmbrookstudents.org>
2022-03-04 18:47:28 -0600

    Value set from SmartDashboard entry

--- Dio0_on/src/main/java/frc/robot/Robot.java
+++ Dio0_on/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -18,6 +20,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private DigitalOutput m_dio;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -74,9 +77,11 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    DigitalOutput m_dio = new DigitalOutput(0);
+    m_dio = new DigitalOutput(0);
     m_dio.set(true);
     
+    SmartDashboard.putBoolean("DIO on (toggle)", true);
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -84,7 +89,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    m_dio.set(SmartDashboard.getBoolean("Dio on (toggle)", true));
+  }
 
   @Override
   public void testInit() {
